d5bfe84ad3c97b5f89cbf66ec60b49fa362c8bde
Al3dekev <36031463+Al3dekev@users.noreply.github.com>
2018-07-08 11:55:54 +0200

    Nouveauté(Entities): Mise a jour des attributs et get/set inutile ou répété

--- src/main/java/entities/GameNPC.java
+++ src/main/java/entities/GameNPC.java
@@ -2,21 +2,10 @@ package entities;
 
 public class GameNPC extends GameEntities {
 
-    private int npcIndex;
 
     public GameNPC(){
         this.setNpcStatus(true);
     }
-
-    /*** LISTAGE SET/GET ***/
-
-    public int getNpcIndex(){
-        return this.npcIndex;
-    }
-    public void setNpcIndex(int set){
-        this.npcIndex = set;
-    }
-
 
 
 }
--- src/main/java/entities/GamePC.java
+++ src/main/java/entities/GamePC.java
@@ -2,19 +2,9 @@ package entities;
 
 public class GamePC extends GameEntities {
 
-    private int playerIndex;
-    private int PS;
 
     public GamePC(){
 
     }
 
-    /*** LISTAGE GET/SET ***/
-
-    public int getPS(){
-        return this.PS;
-    }
-    public void setPS(int set){
-        this.PS = set;
-    }
 }
