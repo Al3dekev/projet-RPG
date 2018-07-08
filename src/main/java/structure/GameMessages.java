2148d20d128050f716e346d42afda882cf724c7f
Al3dekev <36031463+Al3dekev@users.noreply.github.com>
2018-07-08 14:02:00 +0200

    Nouveauté(GameMessages): Préparation de base de GameMessages

--- src/main/java/structure/GameMessages.java
+++ src/main/java/structure/GameMessages.java
@@ -2,7 +2,31 @@ package structure;
 
 public class GameMessages {
 
-    
+    public GameMessages(){
 
+    }
+
+    /***
+     * Gère le message de base visualisé a chaque tour
+     */
+    public void eachTurnMessages(){
+
+    }
+
+    public void actionAndTargetMessage(){
+
+    }
+
+    public void victoryMessage(){
+
+    }
+
+    public void monsterSuperHitMessage(){
+
+    }
+
+    public void numberTurnMessage(){
+
+    }
 
 }
